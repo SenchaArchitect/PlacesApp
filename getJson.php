eb27d09b4f3f2e08b8ce0b6a56dc87c37f4b4ecd
davestum <stummer@gmail.com>
2013-03-13 16:17:47 +0000

    replace file_get_contents with curl, as the former was causing some php errors

--- getJson.php
+++ getJson.php
@@ -1,5 +1,18 @@
 <?php
 
-echo file_get_contents("https://maps.googleapis.com/maps/api/place/nearbysearch/json?" .
+function curl($url){
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        return curl_exec($curl);
+        curl_close($curl);
+
+}
+
+echo curl("https://maps.googleapis.com/maps/api/place/nearbysearch/json?" .
                        "key={$_GET['key']}&location={$_GET['location']}&radius={$_GET['radius']}" .
                        "&sensor={$_GET['sensor']}&keyword={$_GET['keyword']}");
